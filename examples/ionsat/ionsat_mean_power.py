3710b547c0b1afab17e671ed9a2d4072d70a67e8
Antoine Tavant <antoine.tavant@lpp.polytechnique.fr>
2021-02-23 10:44:08 +0100

    Better scilab and celestlab managment for local architecture and added ionsat small example

---
+++ examples/ionsat/ionsat_mean_power.py
@@ -0,0 +1,34 @@
+import space_mission_design
+from space_mission_design.celestlab import celestlab_wrapper
+from space_mission_design.visualisation import ploting_map
+
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use("presentation")
+
+from astropy import units as u
+
+from poliastro.bodies import Earth, Mars, Sun
+from poliastro.twobody import Orbit
+from tqdm.auto import tqdm
+import os
+
+from space_mission_design.power import body_illumination
+
+wrapper = celestlab_wrapper.WrapperCelestlab(scilab_path="/home/tavant/Data/tavant/CSE-perso/tools/CNES/scilab-6.0.2/bin/",
+                                             celestlab_loader="/home/tavant/Data/tavant/CSE-perso/IonSat/power/loader_celestlab.sce")
+
+print("Small example : propagate and plot")
+specitic_params = {"year":2024, "hour":12, "inc": 51*np.pi/180, "sma": (Earth.R_mean + 300 * u.km).to(u.m).value,
+                   "outputFileName":"results_ionsat.h5" }
+
+wrapper.write_paramerter_file(specitic_params)
+wrapper.launch_celestlab("ionsat_power.sce")
+
+sun_position, cj_date, ecf_position, eclipses = wrapper.read_celestlab_results("results_ionsat.h5")
+
+ploting_map.plot_planisphere(ecf_position)
+
+ploting_map.plot_poles(ecf_position)
+plt.show()
+# plt.savefig("map_51deg.png", dpi=300)
--- space_mission_design/celestlab/celestlab_wrapper.py
+++ space_mission_design/celestlab/celestlab_wrapper.py
@@ -3,88 +3,121 @@ from numpy.linalg import norm
 import h5py as hp
 
 import subprocess
+import os
 
 
-def get_default_params():
-    """return a dict ith the parameters that can be used in celestlab"""
-    params = {
-        "year": 2020,
-        "month": 6,
-        "day": 15,
-        "hour": 12,
-        "minutes": 30,
-        "outputFileName": "results.h5",
-        "sma": 7200.0e3,  # semi  major axis
-        "ecc": 1.0e-3,  # eccentricity
-        "inc": 98 * np.pi / 180,  # inclination
-        "pom": np.pi / 2,  # Argument of perigee
-        "mlh": 10.5,  # MLTAN(hours)(mean  local  time of ascending node))
-        "anm": 0,  # Mean   anomaly
-        "number_days": 1,  # the total number of days simulated
-        "timestep": 3 / 86400,  # The time step of the simulations
-    }
+class WrapperCelestlab:
+    """Object to run selectal from python"""
 
-    return params
+    def __init__(self, scilab_path=None, celestlab_loader="loader_celestlab.sce"):
 
+        self.celestlab_loader = celestlab_loader
 
-def write_paramerter_file(other_params):
-    """Write the parameter_file that is used in CelestLab.
-    Celestlab i/o is shit, so the order of the parameters given is very important"""
+        if not os.path.isdir(scilab_path) :
+            raise RuntimeError("The `scilab_path` argument is not a directory")
 
-    params = get_default_params()
-
-    for k, v in other_params.items():
-        if k in params:
-            params[k] = v
+        if scilab_path is not None:
+            self.scilab_path = scilab_path
         else:
-            raise RuntimeError(
-                f"Parameter {k} is not understood. \n Available parameters are {params.keys()}"
-            )
+            raise RuntimeError("The scilab executable need to be provided")
 
-    filename = "./parameters.txt"
+        if os.name == "nt":
+            """Running on Windows
+            to launch scilab without the window, we need to use the good path"""
+            self.scilab_exec = self.scilab_path + "/WScilex-cli.exe"
 
-    parameter_key_list = [
-        "year",
-        "month",
-        "day",
-        "hour",
-        "minutes",
-        "sma",
-        "ecc",
-        "inc",
-        "mlh",
-        "anm",
-        "number_days",
-        "timestep",
-    ]
+        elif os.name == "posix":
+            """Running on linux
+            We launch with the usual `scilab` and a CLI argument """
+            self.scilab_exec = self.scilab_path + "/scilab -nw"
+            self.scilab_exec = self.scilab_path + "/scilab-cli"
 
-    with open(filename, "w") as f:
-        for k in parameter_key_list:
-            f.write(f"{params[k]}\n")
+        self.celestlab_exec = "Celestlab_loader="+self.celestlab_loader+" "+self.scilab_exec
+
+        print(self.celestlab_exec)
+
+    def get_default_params(self):
+        """return a dict ith the parameters that can be used in celestlab"""
+        params = {
+            "year": 2020,
+            "month": 6,
+            "day": 15,
+            "hour": 12,
+            "minutes": 30,
+            "outputFileName": "results.h5",
+            "sma": 7200.0e3,  # semi  major axis
+            "ecc": 1.0e-3,  # eccentricity
+            "inc": 98 * np.pi / 180,  # inclination
+            "pom": np.pi / 2,  # Argument of perigee
+            "mlh": 10.5,  # MLTAN(hours)(mean  local  time of ascending node))
+            "anm": 0,  # Mean   anomaly
+            "number_days": 1,  # the total number of days simulated
+            "timestep": 3 / 86400,  # The time step of the simulations
+        }
+
+        return params
 
 
-def launch_celestlab():
-    """launch the celestlab script with subprocess"""
+    def write_paramerter_file(self, other_params):
+        """Write the parameter_file that is used in CelestLab.
+        Celestlab i/o is shit, so the order of the parameters given is very important"""
 
-    command = "/home/tavant/Documents/CSE-perso/tools/CNES/scilab-6.0.2/bin/scilab -nw -f crocus_power.sce "
-    env = {
-        "LIBGL_ALWAYS_SOFTWARE": "1"
-    }  # Should be used to disable the hardware accelleration of libGL used by Scilab
+        params = self.get_default_params()
 
-    p = subprocess.Popen(command.split())
-    p.wait()
+        for k, v in other_params.items():
+            if k in params:
+                params[k] = v
+            else:
+                raise RuntimeError(
+                    f"Parameter {k} is not understood. \n Available parameters are {params.keys()}"
+                )
+
+        filename = "./parameters.txt"
+
+        parameter_key_list = [
+            "year",
+            "month",
+            "day",
+            "hour",
+            "minutes",
+            "sma",
+            "ecc",
+            "inc",
+            "mlh",
+            "anm",
+            "number_days",
+            "timestep",
+            "outputFileName",
+        ]
+
+        with open(filename, "w") as f:
+            for k in parameter_key_list:
+                f.write(f"{params[k]}\n")
 
 
-def read_celestlab_results(filename="./results.h5"):
-    """The celestlab script dumps the results in a HDF5 file. """
+    def launch_celestlab(self, scriptname="crocus_power.sce"):
+        """launch the celestlab script with subprocess"""
 
-    with hp.File(filename, "r") as f:
-        # print(f.keys())
-        sun_dir = f["Sun_dir"][()]
-        cjd = f["cjd"][()]
-        pos_ecf = f["pos_ecf"][()]
-        eclipses = f["interv"][()]
+        command = self.scilab_exec + " -f "+scriptname
+        my_env = os.environ.copy()
 
-    cjd = cjd[:, 0]
+        my_env["LIBGL_ALWAYS_SOFTWARE"] = "1"   # Should be used to disable the hardware accelleration of libGL used by Scilab
+        my_env["Celestlab_loader"] = self.celestlab_loader
 
-    return sun_dir, cjd, pos_ecf, eclipses
+        p = subprocess.Popen(command.split(), env=my_env)
+        p.wait()
+
+
+    def read_celestlab_results(self, filename="./results.h5"):
+        """The celestlab script dumps the results in a HDF5 file. """
+
+        with hp.File(filename, "r") as f:
+            # print(f.keys())
+            sun_dir = f["Sun_dir"][()]
+            cjd = f["cjd"][()]
+            pos_ecf = f["pos_ecf"][()]
+            eclipses = f["interv"][()]
+
+        cjd = cjd[:, 0]
+
+        return sun_dir, cjd, pos_ecf, eclipses
